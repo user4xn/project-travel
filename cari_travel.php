2ef6be4c136fe933de871c0d56427a307d57d00c
user4xn <wildan.desperado@gmail.com>
2021-08-27 15:24:15 +0700

    Fix Footer dan Cari Travel

---
+++ cari_travel.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title>Cari | Baritrans - Tour & Travel</title>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/newfontawesome/css/all.css">
+     <link rel="stylesheet" href="css/aos.css">
+     <link rel="stylesheet" href="css/h_image.css">
+     <link rel="stylesheet" href="nice-select/css/nice-select.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/tooplate-gymso-style.css">
+     <style type="text/css">
+       ul.list{
+        max-height: 200px !important;
+        overflow-y: scroll !important;
+        z-index: 2;
+       }
+     </style>
+</head>
+<body data-spy="scroll" data-target="#navbarNav" data-offset="50">
+	
+    <!-- WHATSAPP LINK -->
+    <a href="https://wa.me/+6281227082077?text=Halo%20Saya%20Ingin%20Pesan%20Travel" target="_blank" class="float">
+        <i class="fab fa-whatsapp my-float"></i>
+    </a>
+    <!-- MENU BAR -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php"><span class="text-color">B</span>aritrans.</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-lg-auto">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link smoothScroll">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="cari_travel.php" class="nav-link smoothScroll">Cari Travel</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="tentang_kami.php" class="nav-link smoothScroll">Tentang Kami</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section class="feature pt-4 pb-4 mt-5" id="feature">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto col-12">
+                    <div class="hero-text mt-5 text-center" data-aos="fade-down" data-aos-delay="400">
+                        <h3 class="text-white mb-4">HASIL PENCARIAN <span class="main-title">TOUR</span></h3>
+                        <form>                
+                        <div class="row pl-4 pr-4">
+                            <div class="col-md-4 pr-1 pl-1">
+                                <select class="wide mb-3">
+                                  <option selected>Semua Kategori</option>
+                                  <option value="Banjar Negara">Banjar Negara</option>
+                                  <option value="Purbalingga">Purbalingga</option>
+                                  <option value="Cilacap">Cilacap</option>
+                                  <option value="Purwokerto">Purwokerto</option>
+                                  <option value="Wonosobo">Wonosobo</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4 pr-1 pl-1">
+                                <select class="wide mb-3">
+                                  <option selected>Semua Destinasi</option>
+                                  <option value="bandung">Bandung</option>
+                                  <option value="jakarta">Jakarta</option>
+                                  <option value="jepara">Jepara</option>
+                                  <option value="jogja">Jogja</option>
+                                  <option value="kudus">Kudus</option>
+                                  <option value="pati">Pati</option>
+                                  <option value="purwodadi">Purwodadi</option>
+                                  <option value="semarang">Semarang</option>
+                                  <option value="solo">Solo</option>
+                                  <option value="wonosobo">Wonosobo</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4 pr-1 pl-1">
+                                <select class="wide mb-3">
+                                  <option selected>Semua Durasi</option>
+                                  <option value="08.00">08.00</option>
+                                  <option value="16.00">16.00</option>
+                                  <option value="20.00">20.00</option>
+                                  <option value="22.00">22.00</option>
+                                </select>
+                            </div>
+                        </div>
+                        <input type="submit" class="btn custom-btn bordered mt-3" value="Cari Tour!">
+                        </form>
+                    </div>
+               </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- TOUR -->
+    <section class="class section" id="class">
+        <div class="container">
+             <div class="row">
+                    <div class="col-lg-12 col-12 text-left mb-5">
+                        <!-- KIE AJA KELALEN YAMAS -->
+                        <h6 data-aos="fade-up"><u>6</u> hasil pencarian ditemukan!</h6>
+                    </div>
+                    <!-- LOOPING ,GANTI MAS YA :V -->
+                    <?php for ($i=0; $i < 6; $i++) {  ?>
+                    <!-- SINGLE ITEM -->
+                    <div class="col-lg-4 col-md-6 col-12" data-aos="fade-up" data-aos-delay="100">
+                        <div class="class-thumb">
+                            <div class="c_img">
+                              <img src="images/class/travel_car.jpg" class="img-fluid" alt="Class">
+                              <div class="overlay">
+                                <!-- LINK -->
+                                <a class="text btn btn-outline-warning btn-sm" href="detail_tour.php">Lihat Detail</a>
+                              </div>
+                            </div>
+                            <div class="class-info pr-0 pl-0">
+                                <strong class="text-dark">
+                                    <h4 class="mb-1">Purbalingga - Jogja - Solo (PP)</h4>
+                                </strong>
+                                <span class="text-color"><i class="fas fa-map-marker-alt"></i> Purbalingga - <i class="fas fa-clock"></i> 08.00</span>
+                                <p class="mt-3"><b>Rp 300.000</b>/kursi</p>
+                            </div>
+                        </div>
+                    </div>
+                    <?php } ?>
+             </div>
+        </div>
+    </section>
+
+
+    <!-- FOOTER -->
+    <section class="schedule section pt-3 pb-2" id="schedule">
+              <div class="container">
+                   <div class="row">
+                       <div class=" col-lg-12 col-md-12 text-center">
+                           <p class="copyright-text">Copyright &copy; <script type="text/javascript">document.write(new Date().getFullYear());</script> Baritrans</p>
+                       </div>
+                   </div>
+              </div>
+    </section>
+
+    <!-- SCRIPTS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="nice-select/js/jquery.js"></script>
+    <script src="nice-select/js/jquery.nice-select.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+         $('select').niceSelect();
+      });
+    </script>
+</body> 
+</html>
---
+++ index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title>Baritrans - Tour & Travel</title>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/newfontawesome/css/all.css">
+     <link rel="stylesheet" href="css/aos.css">
+     <link rel="stylesheet" href="css/h_image.css">
+     <link rel="stylesheet" href="nice-select/css/nice-select.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/tooplate-gymso-style.css">
+     <style type="text/css">
+       ul.list{
+        max-height: 200px !important;
+        overflow-y: scroll !important;
+        z-index: 2;
+       }
+     </style>
+</head>
+<body data-spy="scroll" data-target="#navbarNav" data-offset="50">
+	  
+    <!-- WHATSAPP LINK -->
+    <a href="https://wa.me/+6281227082077?text=Halo%20Saya%20Ingin%20Pesan%20Travel" target="_blank" class="float">
+        <i class="fab fa-whatsapp my-float"></i>
+    </a>
+    <!-- MENU BAR -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php"><span class="text-color">B</span>aritrans.</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-lg-auto">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link smoothScroll">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="cari_travel.php" class="nav-link smoothScroll">Cari Travel</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="tentang_kami.php" class="nav-link smoothScroll">Tentang Kami</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+     <!-- HERO -->
+     <section class="hero d-flex flex-column justify-content-center align-items-center" id="home">
+        <div class="bg-overlay"></div>
+        <div class="container">
+             <div class="row">
+                  <div class="col-lg-8 col-md-10 mx-auto col-12">
+                       <div class="hero-text mt-5 text-center" data-aos="fade-up" data-aos-delay="400">
+                             <h6>Selamat Datang di Baritrans!</h6>
+                             <h1 class="text-white">Cari Rute Travel</h1>
+                             <form>
+                             <div class="row pl-5 pr-5">
+                                 <div class="col-md-4 pr-1 pl-1">
+                                     <select class="wide mb-3">
+                                       <option selected>Semua Kategori</option>
+                                       <option value="Banjar Negara">Banjar Negara</option>
+                                       <option value="Purbalingga">Purbalingga</option>
+                                       <option value="Cilacap">Cilacap</option>
+                                       <option value="Purwokerto">Purwokerto</option>
+                                       <option value="Wonosobo">Wonosobo</option>
+                                     </select>
+                                 </div>
+                                 <div class="col-md-4 pr-1 pl-1">
+                                     <select class="wide mb-3">
+                                       <option selected>Semua Destinasi</option>
+                                       <option value="bandung">Bandung</option>
+                                       <option value="jakarta">Jakarta</option>
+                                       <option value="jepara">Jepara</option>
+                                       <option value="jogja">Jogja</option>
+                                       <option value="kudus">Kudus</option>
+                                       <option value="pati">Pati</option>
+                                       <option value="purwodadi">Purwodadi</option>
+                                       <option value="semarang">Semarang</option>
+                                       <option value="solo">Solo</option>
+                                       <option value="wonosobo">Wonosobo</option>
+                                     </select>
+                                 </div>
+                                 <div class="col-md-4 pr-1 pl-1">
+                                     <select class="wide mb-3">
+                                       <option selected>Semua Durasi</option>
+                                       <option value="08.00">08.00</option>
+                                       <option value="16.00">16.00</option>
+                                       <option value="20.00">20.00</option>
+                                       <option value="22.00">22.00</option>
+                                     </select>
+                                 </div>
+                             </div>
+                             <input type="submit" class="btn custom-btn bordered mt-3" value="Cari Tour!">
+                             </form>
+                       </div>
+                  </div>
+             </div>
+        </div>
+     </section>
+
+     <section class="feature pt-4 pb-4" id="feature">
+        <div class="container">
+            <div class="row">
+                <div class="d-flex flex-column justify-content-center ml-lg-auto mr-lg-5 col-lg-5 col-md-6 col-sm-6 col-12">
+                    <h2 class="mb-2 text-white" data-aos="fade-right" data-aos-delay="400">Alamat <i class="ml-2 far fa-map"></i></h2>
+                    <p class="pb-0" data-aos="fade-right" data-aos-delay="500">Jl. Wirayuda No.5 Purbalingga Kidul, Kab.Purbalingga - Jawa Tengah</p>
+                    <a href="https://goo.gl/maps/F8dZ6M8VbeEBngon6" target="_blank" class="btn custom-btn bg-color mt-0" data-aos="fade-right" data-aos-delay="500">Bawa Saya Kesana!</a>
+                </div>
+                <div class="mr-lg-auto mt-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                     <div class="about-working-hours">
+                          <div>
+                              <h2 class="mb-2 text-white" data-aos="fade-left" data-aos-delay="300">Hotline <i class="ml-2 fab fa-whatsapp"></i></h2>
+                              <p data-aos="fade-left" data-aos-delay="500">0812-2708-2077</p>
+                              <p data-aos="fade-left" data-aos-delay="500">Untuk informasi yang lebih lanjut silahkan <a class="text-color" href="https://wa.me/+6281227082077?text=Halo%20Saya%20Ingin%20Bertanya">hubungi kami</a>.</p>
+                          </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ABOUT -->
+    <section class="about section" id="about">
+        <div class="container">
+             <div class="row">
+                  <div class="mt-lg-5 mb-lg-0 mb-4 col-lg-5 col-md-10 mx-auto col-12">
+                      <h2 class="mb-4" data-aos="fade-up" data-aos-delay="300">Halo, Kami adalah Baritrans</h2>
+                      <p data-aos="fade-up" data-aos-delay="400">Baritrans merupakan sebuah perusahaan moda transportasi umum untuk berbagai kepentingan seperti Travel - Charter, selain transportasi kami juga melayani pengiriman paket kilat untuk barang dan dokumen</p>
+                      <p data-aos="fade-up" data-aos-delay="500">Lokasi kami berada di kota Purbalingga dan siap melayani ke berbagai kota tujuan</p>
+                  </div>
+                  <div class="mt-lg-5 mb-lg-0 mb-4 col-lg-5 col-md-10 mx-auto col-12" data-aos="fade-up" data-aos-delay="700">
+                      <div class="team-thumb pt-2">
+                          <img src="images/baritrans.jpeg" class="img-fluid" alt="baritrans">
+                      </div>
+                  </div>
+             </div>
+        </div>
+    </section>
+
+     <!-- JASA -->
+    <section class="schedule section" id="class">
+        <div class="container">
+             <div class="row text-center">
+                 <div class="col-md-4 pt-3" data-aos="fade-right" data-aos-delay="300">
+                     <div class="bordered rounded p-5">
+                         <h1 class="mb-1"><i class="fas fa-shipping-fast"></i></h1>
+                         <h3 class="mb-3">PAKET KILAT</h3>
+                         <p class="text-white">Menerima paket barang maupun dokumen ke berbagai kota dengan sistem kilat</p>
+                     </div>
+                 </div>
+                 <div class="col-md-4 pt-3" data-aos="fade-right" data-aos-delay="200">
+                     <div class="bordered rounded p-5">
+                         <h1 class="mb-1"><i class="fas fa-route"></i></h1>
+                         <h3 class="mb-3">TRAVEL</h3>
+                         <p class="text-white">Purbalingga, Semarang, Demak, Jakarta, Pati, Grobogan, Purwodadi, Banjarnegara, Wonosobo, Temanggung, Jogja, Solo</p>
+                     </div>
+                 </div>
+                 <div class="col-md-4 pt-3" data-aos="fade-right" data-aos-delay="100">
+                     <div class="bordered rounded p-5">
+                         <h1 class="mb-1"><i class="fas fa-car-side"></i></h1>
+                         <h3 class="mb-3">CARTER</h3>
+                         <p class="text-white">Melayani carter dengan berbagai moda transportasi dengan jenis VAN, MPV, dan Big Bus</p>
+                     </div>
+                 </div>
+             </div>
+        </div>
+    </section>
+
+    <!-- TOUR -->
+    <section class="class section" id="class">
+        <div class="container">
+             <div class="row">
+                  <div class="col-lg-12 col-12 text-center mb-5">
+                      <h6 data-aos="fade-up">Dapatkan Perjalanan Yang Nyaman</h6>
+                      <h2 data-aos="fade-up" data-aos-delay="200"><span class="main-title">Tour</span> Terbaru</h2>
+                  </div>
+                  <!-- LOOPING ,GANTI MAS YA :V -->
+                  <?php for ($i=0; $i < 6; $i++) {  ?>
+                  <!-- SINGLE ITEM -->
+                  <div class="col-lg-4 col-md-6 col-12" data-aos="fade-up" data-aos-delay="100">
+                      <div class="class-thumb">
+                          <div class="c_img">
+                            <img src="images/class/travel_car.jpg" class="img-fluid" alt="Class">
+                            <div class="overlay">
+                              <!-- LINK -->
+                              <a class="text btn btn-outline-warning btn-sm" href="detail_tour.php">Lihat Detail</a>
+                            </div>
+                          </div>
+                          <div class="class-info pr-0 pl-0">
+                              <strong class="text-dark">
+                                  <h4 class="mb-1">Purbalingga - Jogja - Solo (PP)</h4>
+                              </strong>
+                              <span class="text-color"><i class="fas fa-map-marker-alt"></i> Purbalingga - <i class="fas fa-clock"></i> 08.00</span>
+                              <p class="mt-3"><b>Rp 300.000</b>/kursi</p>
+                          </div>
+                      </div>
+                  </div>
+                  <?php } ?>
+             </div>
+        </div>
+    </section>
+
+    <!-- ARTIKEL -->
+    <section class="class section" id="class">
+        <div class="container">
+             <div class="row">
+                  <div class="col-lg-12 col-12 text-center mb-5">
+                      <h6 data-aos="fade-up">Baca juga informasi menarik, tips & trik seputar traveling berikut ini!</h6>
+                      <h2 data-aos="fade-up" data-aos-delay="200"><span class="main-title">Artikel</span> Terbaru</h2>
+                   </div>
+                  <div class="col-lg-12 col-md-12 col-12" data-aos="fade-up" data-aos-delay="200">
+                      <div class="class-thumb">
+                          <!-- LOOPING ,MANING :V -->
+                          <?php for ($i=0; $i < 2; $i++) {  ?>
+                          <!-- SINGLE ITEM -->
+                          <a href="artikel.php">
+                          <div class="row mt-5 mb-5">
+                            <!-- LINK POST -->
+                              <div class="col-md-3">
+                                  <img src="images/class/travel_car.jpg" class="img-fluid" alt="Class" style="height: 100%;object-fit: cover;">
+                              </div>
+                              <div class="col-md-9">  
+                                  <div class="p-0">
+                                      <!-- TITLE POST -->
+                                      <strong class="text-dark">
+                                          <h3 class="mb-1">Travel Rute Jawa Tengah Antar Jemput</h3>
+                                      </strong>
+                                      <!-- ID POST -->
+                                      <span class="blockquote-footer mt-2">
+                                          <i class="fas fa-calendar-alt"></i> 22 Agustus 2021 &nbsp;&nbsp;
+                                          <i class="fas fa-eye"></i> 10x  &nbsp;&nbsp;
+                                          <i class="fas fa-folder-open"></i> Tour & Travel  &nbsp;&nbsp;
+                                      </span>
+                                      <!-- TEXT POST -->
+                                      <p class="mt-2">
+                                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                          consequat.
+                                      </p>
+                                  </div>
+                              </div>
+                          </div>
+                          </a>
+                          <hr>
+                          <?php } ?>
+                      </div>
+                  </div>
+             </div>
+        </div>
+    </section>
+
+
+    <!-- FOOTER -->
+  <section class="schedule section pt-3 pb-2" id="schedule">
+            <div class="container">
+                 <div class="row">
+                     <div class=" col-lg-12 col-md-12 text-center">
+                         <p class="copyright-text">Copyright &copy; <script type="text/javascript">document.write(new Date().getFullYear());</script> Baritrans</p>
+                     </div>
+                 </div>
+            </div>
+  </section>
+
+    <!-- SCRIPTS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="nice-select/js/jquery.js"></script>
+    <script src="nice-select/js/jquery.nice-select.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+         $('select').niceSelect();
+      });
+    </script>
+</body> 
+</html>
